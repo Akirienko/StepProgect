d526642ded81054a124857ae97a0ce530048ce76
Anton <lakirienkol@gmail.com>
2020-10-17 16:55:12 +0300

    The end

--- js/script.js
+++ js/script.js
@@ -12,7 +12,9 @@ tabs.addEventListener("click", function(el) {
   });
 });
 
-/////////////////////                  Our Amazing Work
+/**************************\
+  Start Our Amazing Work
+\**************************/                
 
 const tabsListWork = document.querySelectorAll(".tabs-list");
 const tabsWorkAll = document.querySelector(".tabsImg-all");
@@ -168,12 +170,59 @@ const allImg = [
 const createImg = (array) => {
   array.forEach(el => {
     let img = document.createElement("img");
+    let tabsImgList = document.createElement("div");
+    // div hover and his content
+    let divHover = document.createElement("div");
+    let divHoverItemsSrc = document.createElement("div");
+    let divHoverSrcLeft = document.createElement("a");
+    let divHoverSrcRight = document.createElement("a");
+    let divHoverTitleName = document.createElement("h3");
+    let divHoverSpan = document.createElement("span");
+    let divHoverItemLeft = document.createElement("div");
+    let divHoverItemRight = document.createElement("div");
+    let divHoverItemRightSquare = document.createElement("div");
+
+
     img.src = el.imgSrc;
-    img.setAttribute( "data-filter", el.type);
-    img.classList.add("activeWork");
-    tabsWorkAll.appendChild(img);
+    tabsImgList.setAttribute( "data-filter", el.type);
+    tabsImgList.classList.add("activeWork");
+    tabsWorkAll.appendChild(tabsImgList);
+    tabsImgList.appendChild(img);
+    tabsImgList.classList.add("tabsImgList");
+
+    // Добовляем див ховер его контент и классы 
+    tabsImgList.appendChild(divHover);
+    divHover.classList.add("divHover");
+
+    divHover.appendChild(divHoverItemsSrc);
+    divHoverItemsSrc.classList.add("divHoverItemsSrc");
+
+    divHoverItemsSrc.appendChild(divHoverSrcLeft);
+    divHoverSrcLeft.classList.add("divHoverSrcLeft");
+    divHoverSrcLeft.appendChild(divHoverItemLeft);
+    divHoverItemLeft.classList.add("divHoverItemLeft");
+    // divHoverTitleName.innerHTML = " ";
+
+    divHoverItemsSrc.appendChild(divHoverSrcRight);
+    divHoverSrcRight.classList.add("divHoverSrcRight");
+    divHoverSrcRight.appendChild(divHoverItemRight);
+    divHoverItemRight.classList.add("divHoverItemRight");
+    divHoverItemRight.appendChild(divHoverItemRightSquare);
+    divHoverItemRightSquare.classList.add("divHoverItemRightSquare");
+    // divHoverTitleName.innerHTML = " ";
+
+    divHover.appendChild(divHoverTitleName);
+    // divHoverTitleName.classList.add("divHoverTitleName");
+    divHoverTitleName.innerHTML = "creative design";
+
+    divHover.appendChild(divHoverSpan);
+    // divHoverSpan.classList.add("divHoverSpan");
+    divHoverSpan.innerHTML = "Web Design";
+    
+    
   });
 };
+
 
 const filterAdd = (arr, attribute) => {
   // console.log(arr, attribute);
@@ -187,64 +236,143 @@ const filterAdd = (arr, attribute) => {
 };
 
 const allImgArray = allImg.slice(0 , 12);
-let firstImgSorted = allImgArray.sort(() => Math.random() - 0.5);
+let firstImgSorted = allImgArray.sort();
 
 createImg(firstImgSorted);
+// checkBtnLoad(true);
 
 tabsListWork.forEach(el => {
   el.addEventListener("click", (ev) => {
 
-    let tabNavAttr = ev.target.getAttribute("data-typeWork");
-    let del = document.querySelectorAll(".tabsImg-all img");
-    // Удаляем класс activeWork с картинок 
+  let tabNavAttr = ev.target.getAttribute("data-typeWork");
+	let del = document.querySelectorAll(".tabsImg-all .tabsImgList");
+
+	// removing class activeWork from active tab
+	document.querySelector(".tabs-list.activeWork").classList.remove("activeWork");
+	// adding ativeWork class to current tab
+	ev.target.classList.add("activeWork");
+
+	if(tabNavAttr === 'all'){
+    createImg(firstImgSorted);
+    document.querySelector("#workImgLoad").classList.add("activeLoad");
+    return;
+    
+    // checkBtnLoad(true);
+  }
+  
+
+
+	// Удаляем класс activeWork с картинок	
     del.forEach(el => {
       if (el.classList.contains("activeWork")) {
         el.classList.remove("activeWork");
       }
     })
-    // Отфильтровали массив allImg по tabNavAttr
-    let addResult = filterAdd(allImg, tabNavAttr);
-    // rerender view
-    createImg(addResult);
+  // Отфильтровали массив allImg по tabNavAttr
+	let addResult = filterAdd(allImg, tabNavAttr);
 
-    console.log(ev.target);
+	// check if for some reason button "load more" is hidden
+	// checkBtnLoad();
+	
+	// rerender view
+	createImg(addResult);
+  document.querySelector("#workImgLoad").classList.remove("activeLoad");
+	
+
+    // console.log(ev.target);
   })
 }); 
 
-// tabsListWork.forEach(item =>
-//       item.classList.remove("activeWork"));
-//       ev.target.classList.add("activeWork");
 
-    
-//     if(el.target.dataset.type === "all"){
-//       createImg(allImgArray);
-//       console.log(allImgArray);
-//     }
    
 workImgLoad.addEventListener("click", () => {
-  let arrMoreImg = firstImgSorted.concat(allImg.slice(12, 24));
-  createImg(arrMoreImg);
+
+	// grab current images and coumt starting pointr of new array 
+	let currentImgs = document.querySelectorAll(".tabsImg-all .tabsImgList.activeWork").length;
+	console.log(currentImgs);
+	// check if current length not bigger than original array of imgs lenght 
+	// if true hide button 
+	if(currentImgs >= allImg.length - 12) {
+		workImgLoad.style.display = "none";
+	}
+
+	// create new piece of array and print it 
+	let newImgs = allImg.slice(currentImgs, currentImgs + 12);
+
+	createImg(newImgs);
 })
 
 
-const mySwiper = new Swiper('.gallery-top', {
-  spaceBetween: 6,
-  thumbs: {
-    swiper: {
-      el: '.gallery-thumbs',
-      spaceBetween: 6,
-      slidesPerView: 4,
-      freeMode: true } },
-    loop: true,
+//////////////// End Our Amazing Work
 
 
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev' } 
+
+
+/**************************\
+  Start SLIDER
+\**************************/   
+
+const sliderBtnPrev = document.querySelector("#slider-btn_prev");
+const sliderBtnNext = document.querySelector("#slider-btn_next");
+const persons = document.querySelectorAll(".person-info_item");
+const slider = document.querySelector(".slider-wrapper");
+const sliderItem = document.querySelectorAll(".slider-item");
+
+let count = 0;
+let countImage = document.querySelector(".slider-active");
+
+
+slider.addEventListener("click", (ev) => {
+  /// Удаляем класс slider-active
+  sliderItem.forEach(item => {
+    if(item.classList.contains("slider-active")) {
+      item.classList.remove("slider-active")
+    }
+  })
+  /// Добовляем класс slider-active
+  ev.target.closest("li").classList.add("slider-active");
+
+  persons.forEach((el, index) => {
+    if(ev.target.closest("li").dataset.name === el.dataset.name) {
+      el.classList.add("is-active");
+      count = index;
+    } else {
+      el.classList.remove("is-active");
+    }
+  })
 });
 
 
+sliderBtnNext.addEventListener("click", function() {
+  persons[count].classList.remove("is-active");
+  sliderItem[count].classList.remove("slider-active");
+  count++;
+  if (count === sliderItem.length) {
+    count = 0;
+  }
+  sliderItem[count].classList.add("slider-active");
+  persons[count].classList.add("is-active");
+})
 
+sliderBtnPrev.addEventListener("click", function() {
+  persons[count].classList.remove("is-active");
+  sliderItem[count].classList.remove("slider-active");
+  count--;
+  if (count < 0) {
+    count = 3;
+  }
+  sliderItem[count].classList.add("slider-active");
+  persons[count].classList.add("is-active");
+})
+
+
+//////    END SLIDER
+
+
+
+/**************************\
+  Start SLIDER
+\**************************/ 
 
 
 MicroModal.init({
